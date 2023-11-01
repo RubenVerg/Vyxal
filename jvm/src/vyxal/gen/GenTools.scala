8eb3a72a4e1824e5ae5a89a1eb3ef08250d8425a
GingerIndustries <GingerIndustries@users.noreply.github.com>
2023-11-01 15:50:44 +0000

    Scalafix changes

--- jvm/src/vyxal/gen/GenTools.scala
+++ jvm/src/vyxal/gen/GenTools.scala
@@ -1,9 +1,23 @@
 package vyxal.gen
 
 private[vyxal] object GenTools:
-    val templateRegex = raw"\{\{(\w+?)\}\}".r
-    def template(input: String, map: Map[String, Object]): String =
-        val replacements = templateRegex.findAllMatchIn(input).map(m => 
-            m -> map.get(m.group(1)) // fuck you
-        ).filter(_._2.isDefined).map(p => p._1 -> p._2.get)
-        replacements.scanLeft(input)((s, m) => s.patch(m._1.start + (s.size - input.size), m._2.toString, m._1.matched.size)).toList.last
+  val templateRegex = raw"\{\{(\w+?)\}\}".r
+  def template(input: String, map: Map[String, Object]): String =
+    val replacements = templateRegex
+      .findAllMatchIn(input)
+      .map(m => m -> map.get(m.group(1)) // fuck you
+      )
+      .filter(_._2.isDefined)
+      .map(p => p._1 -> p._2.get)
+    replacements
+      .scanLeft(input)((s, m) =>
+        s.patch(
+          m._1.start + (s.size - input.size),
+          m._2.toString,
+          m._1.matched.size,
+        )
+      )
+      .toList
+      .last
+  end template
+end GenTools
